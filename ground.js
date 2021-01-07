e7bb91e9a64785b8aa925a0465550bef9808d55f
vageeshn <75984859+vageeshn@users.noreply.github.com>
2021-01-07 18:03:53 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,15 @@
+class ground{
+    constructor(x,y,width,height){
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+        
+    }
+}
